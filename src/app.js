df7c60bd0403b21d58ff97c0e5209e5623ac4920
Teolin Codreanu <t.codreanu@rankingcoach.com>
2024-05-05 22:11:06 +0300

    chore: same app.js

--- src/app.js
+++ src/app.js
@@ -4,15 +4,17 @@ const taskRouter = require('./modules/task/task.controller')
 const listRouter = require('./modules/list/list.controller')
 const DatabaseService = require('./modules/database/database.service')
 
+// ROUTER
 const app = express()
 const PORT = 3333
 
+// MIDDLEWARE
 app.use(express.json())
-
 app.use('/users', userRouter)
 app.use('/tasks', taskRouter)
 app.use('/lists', listRouter)
 
+// SERVER
 DatabaseService.init()
 app.listen(PORT, () =>
 	console.log(`Server is running on http://localhost:${PORT}`)
