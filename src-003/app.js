5cc9593a1a2c6188893526642a6b15bd80397dad
teolin <teolin@gmail.com>
2024-06-09 00:02:41 +0300

    fix: exceptions

--- src-003/app.js
+++ src-003/app.js
@@ -15,10 +15,10 @@ class UserController {
   static retrieveUser(req, res) {
     try {
       const user = UserService.retrieveUser(req.params.id);
-      if (!user) return res.status(404).json({ message: 'User not found' });
       res.json(user);
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      if (error.message === 'Not Found') res.status(404).json({ message: 'User not found' });
+      else res.status(500).json({ message: error.message });
     }
   }
   static updateUser(req, res) {
@@ -26,7 +26,8 @@ class UserController {
       const user = UserService.updateUser(req.params.id, req.body);
       res.json(user);
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      if (error.message === 'Not Found') res.status(404).json({ message: 'User not found' });
+      else res.status(500).json({ message: error.message });
     }
   }
   static deleteUser(req, res) {
@@ -34,7 +35,8 @@ class UserController {
       UserService.deleteUser(req.params.id);
       res.json({ message: 'User deleted successfully' });
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      if (error.message === 'Not Found') res.status(404).json({ message: 'User not found' });
+      else res.status(500).json({ message: error.message });
     }
   }
 }
@@ -50,18 +52,19 @@ class TaskController {
   static retrieveTask(req, res) {
     try {
       const task = TaskService.retrieveTask(req.params.id);
-      if (!task) return res.status(404).json({ message: 'Task not found' });
       res.json(task);
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      if (error.message === 'Not Found') res.status(404).json({ message: 'Task not found' });
+      else res.status(500).json({ message: error.message });
     }
   }
   static updateTask(req, res) {
     try {
-      const updatedTask = TaskService.updateTask(req.params.id, req.body);
-      res.json(updatedTask);
+      const task = TaskService.updateTask(req.params.id, req.body);
+      res.json(task);
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      if (error.message === 'Not Found') res.status(404).json({ message: 'Task not found' });
+      else res.status(500).json({ message: error.message });
     }
   }
   static deleteTask(req, res) {
@@ -69,17 +72,17 @@ class TaskController {
       TaskService.deleteTask(req.params.id);
       res.json({ message: 'Task deleted successfully' });
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      if (error.message === 'Not Found') res.status(404).json({ message: 'Task not found' });
+      else res.status(500).json({ message: error.message });
     }
   }
   static completeTask(req, res) {
     try {
-      const taskId = req.params.id;
-      const task = TaskService.completeTask(taskId);
-      if (!task) return res.status(404).json({ message: 'Task not found' });
+      const task = TaskService.completeTask(req.params.id);
       res.json(task);
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      if (error.message === 'Not Found') res.status(404).json({ message: 'Task not found' });
+      else res.status(500).json({ message: error.message });
     }
   }
 }
@@ -95,10 +98,10 @@ class ListController {
   static retrieveList(req, res) {
     try {
       const list = ListService.retrieveList(req.params.id);
-      if (!list) return res.status(404).json({ message: 'List not found' });
       res.json(list);
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      if (error.message === 'Not Found') res.status(404).json({ message: 'List not found' });
+      else res.status(500).json({ message: error.message });
     }
   }
   static updateList(req, res) {
@@ -106,7 +109,8 @@ class ListController {
       const updatedList = ListService.updateList(req.params.id, req.body);
       res.json(updatedList);
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      if (error.message === 'Not Found') res.status(404).json({ message: 'List not found' });
+      else res.status(500).json({ message: error.message });
     }
   }
   static deleteList(req, res) {
@@ -114,7 +118,8 @@ class ListController {
       ListService.deleteList(req.params.id);
       res.json({ message: 'List deleted successfully' });
     } catch (error) {
-      res.status(500).json({ message: error.message });
+      if (error.message === 'Not Found') res.status(404).json({ message: 'List not found' });
+      else res.status(500).json({ message: error.message });
     }
   }
 }
@@ -124,61 +129,47 @@ class UserService {
   static createUser(userData) {
     const data = DatabaseService.getData();
     const nextUserId = `U${1 + Number(data.lastUserId.slice(1))}`;
-    const newUser = { userId: nextUserId, ...userData };
-    data.users.push(newUser);
+    const user = { userId: nextUserId, ...userData };
+
+    data.users.push(user);
     data.lastUserId = nextUserId;
     DatabaseService.setData(data);
-    delete newUser.password;
-    return newUser;
+
+    const { password, ...maskedUser } = user;
+    return maskedUser;
   }
+
   static retrieveUser(userId) {
     const data = DatabaseService.getData();
     const user = data.users.find((user) => user.userId === userId);
-    delete user.password;
-    return user;
+
+    if (!user) throw new Error('Not Found');
+    else {
+      const { password, ...maskedUser } = user;
+      return maskedUser;
+    }
   }
+
   static updateUser(userId, userData) {
     const data = DatabaseService.getData();
     const userIndex = data.users.findIndex((user) => user.userId === userId);
-    if (userIndex === -1) throw new Error('User not found');
-    data.users[userIndex] = { ...data.users[userIndex], ...userData };
+    if (userIndex === -1) throw new Error('Not Found');
+    const user = { ...data.users[userIndex], ...userData };
+
+    data.users[userIndex] = user;
     DatabaseService.setData(data);
-    const user = data.users[userIndex];
-    delete user.password;
-    return user;
+
+    const { password, ...maskedUser } = user;
+    return maskedUser;
   }
+
   static deleteUser(userId) {
     const data = DatabaseService.getData();
+    const totalRecords = data.users.length;
+
     data.users = data.users.filter((user) => user.userId !== userId);
-    DatabaseService.setData(data);
-  }
-}
-class ListService {
-  static createList(listData) {
-    const data = DatabaseService.getData();
-    const nextListId = `L${1 + Number(data.lastListId.slice(1))}`;
-    const newList = { listId: nextListId, ...listData };
-    data.lists.push(newList);
-    data.lastListId = nextListId;
-    DatabaseService.setData(data);
-    return newList;
-  }
-  static retrieveList(listId) {
-    const data = DatabaseService.getData();
-    return data.lists.find((list) => list.listId === listId);
-  }
-  static updateList(listId, listData) {
-    const data = DatabaseService.getData();
-    const listIndex = data.lists.findIndex((list) => list.listId === listId);
-    if (listIndex === -1) throw new Error('List not found');
-    data.lists[listIndex] = { ...data.lists[listIndex], ...listData };
-    DatabaseService.setData(data);
-    return data.lists[listIndex];
-  }
-  static deleteList(listId) {
-    const data = DatabaseService.getData();
-    data.lists = data.lists.filter((list) => list.listId !== listId);
-    DatabaseService.setData(data);
+    if (totalRecords === data.users.length) throw new Error('Not Found');
+    else DatabaseService.setData(data);
   }
 }
 class TaskService {
@@ -186,35 +177,95 @@ class TaskService {
     const data = DatabaseService.getData();
     const nextTaskId = `T${1 + Number(data.lastTaskId.slice(1))}`;
     const newTask = { taskId: nextTaskId, ...taskData };
+
     data.tasks.push(newTask);
     data.lastTaskId = nextTaskId;
     DatabaseService.setData(data);
+
     return newTask;
   }
+
   static retrieveTask(taskId) {
     const data = DatabaseService.getData();
-    return data.tasks.find((task) => task.taskId === taskId);
+
+    const task = data.tasks.find((task) => task.taskId === taskId);
+    if (!task) throw new Error('Not Found');
+    else return task;
   }
+
   static updateTask(taskId, taskData) {
     const data = DatabaseService.getData();
     const taskIndex = data.tasks.findIndex((task) => task.taskId === taskId);
-    if (taskIndex === -1) throw new Error('Task not found');
-    data.tasks[taskIndex] = { ...data.tasks[taskIndex], ...taskData };
+    if (taskIndex === -1) throw new Error('Not Found');
+    const updatedTask = { ...data.tasks[taskIndex], ...taskData };
+
+    data.tasks[taskIndex] = updatedTask;
     DatabaseService.setData(data);
-    return data.tasks[taskIndex];
+
+    return updatedTask;
   }
+
   static deleteTask(taskId) {
     const data = DatabaseService.getData();
+    const totalRecords = data.tasks.length;
+
     data.tasks = data.tasks.filter((task) => task.taskId !== taskId);
-    DatabaseService.setData(data);
+    if (totalRecords === data.tasks.length) throw new Error('Not Found');
+    else DatabaseService.setData(data);
   }
+
   static completeTask(taskId) {
     const data = DatabaseService.getData();
     const taskIndex = data.tasks.findIndex((task) => task.taskId === taskId);
-    if (taskIndex === -1) throw new Error('Task not found');
-    data.tasks[taskIndex].isComplete = true;
+
+    if (taskIndex === -1) throw new Error('Not Found');
+    else {
+      data.tasks[taskIndex].isComplete = true;
+      DatabaseService.setData(data);
+      return data.tasks[taskIndex];
+    }
+  }
+}
+class ListService {
+  static createList(listData) {
+    const data = DatabaseService.getData();
+    const nextListId = `L${1 + Number(data.lastListId.slice(1))}`;
+    const newList = { listId: nextListId, ...listData };
+
+    data.lists.push(newList);
+    data.lastListId = nextListId;
     DatabaseService.setData(data);
-    return data.tasks[taskIndex];
+
+    return newList;
+  }
+
+  static retrieveList(listId) {
+    const data = DatabaseService.getData();
+
+    const list = data.lists.find((list) => list.listId === listId);
+    if (!list) throw new Error('Not Found');
+    else return list;
+  }
+
+  static updateList(listId, listData) {
+    const data = DatabaseService.getData();
+    const listIndex = data.lists.findIndex((list) => list.listId === listId);
+    if (listIndex === -1) throw new Error('Not Found');
+    const updatedList = { ...data.lists[listIndex], ...listData };
+
+    data.lists[listIndex] = updatedList;
+    DatabaseService.setData(data);
+
+    return updatedList;
+  }
+
+  static deleteList(listId) {
+    const data = DatabaseService.getData();
+    const totalRecords = data.lists.length;
+
+    data.lists = data.lists.filter((list) => list.listId !== listId);
+    if (totalRecords === data.lists.length) throw new Error('Not Found');
+    else DatabaseService.setData(data);
   }
 }
 
