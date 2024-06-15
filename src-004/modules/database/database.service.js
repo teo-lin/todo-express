baef9cc5d18f9b89169371b27e09e19d7bca7807
teolin <teolin@gmail.com>
2024-06-15 18:43:22 +0300

    feat: alt

--- src-004/modules/database/database.service.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const PATH = path.join(__dirname, './db.json');
-const data = JSON.parse(fs.readFileSync(PATH, 'utf8'));
-
-module.exports = data
